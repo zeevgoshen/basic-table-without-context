9cd12b1c364400f0121094c533adf19a4467fa72
zeevgoshen <zeevgoshen@gmail.com>
2022-06-19 19:20:03 +0300

    Filter component changed to destructure props

--- src/components/table/table.filter.js
+++ src/components/table/table.filter.js
@@ -1,49 +1,51 @@
-import React, { useState, useContext, useMemo } from "react";
-import { IssuesContext } from "../../App";
+import React, { useState, useContext, useMemo } from 'react';
+import { IssuesContext } from '../../App';
 
-import "./table.css";
+import './table.css';
 
 // When the filter text doesn't match, we show the full list of issues
 
-const TableFilter = (props) => {
-  const [filterText, setFilter] = useState("");
+// const TableFilter = (props) => {
+const TableFilter = ({loadIssues, issues, headerText}) => {
+  const [filterText, setFilter] = useState('');
 
   // using this state so we can reload the full issuelist
   // when no results are found
-  const [allIssues, setAllIssues] = useState(props.issues);
+  const [allIssues, setAllIssues] = useState(issues);
 
-  // We will be using setIssues (from context) to update
+  // We will be using setIssues to update
   // the table with the filtered results
-  //const { issues, setIssues } = useContext(IssuesContext);
 
   const filteredIssues = useMemo(() => {
     // no search text in the search textbox
     // show all issues
     if (filterText.length === 0) {
       //setIssues(allIssues);
-      console.log("1")
-      console.log(allIssues)
-      props.loadIssues(allIssues)
+      loadIssues(allIssues);
     }
 
-    return allIssues && allIssues.filter((issue) => {
-      return filterText.length > 0
-        ? issue.selector.toLowerCase().includes(filterText.toLowerCase()) ||
-            issue.url.toLowerCase().includes(filterText.toLowerCase())
-        : allIssues;
-    });
+    return (
+      allIssues &&
+      allIssues.filter((issue) => {
+        console.log(issue.url.toLowerCase());
+        return filterText.length > 0
+          ? issue.selector.toLowerCase().includes(filterText.toLowerCase()) ||
+              issue.url.toLowerCase().includes(filterText.toLowerCase())
+          : allIssues;
+      })
+    );
   }, [filterText, allIssues]);
 
   if (filteredIssues.length > 0 && filteredIssues.length < allIssues.length) {
-    props.loadIssues(filteredIssues);
+    loadIssues(filteredIssues);
   } else if (filteredIssues.length === 0) {
-    props.loadIssues(allIssues);
+    loadIssues(allIssues);
   }
 
   return (
     <div className="filterHeader">
       <label className="filterHeaderLabel">
-        {props.headerText.toUpperCase()}
+        {headerText.toUpperCase()}
       </label>
       <input
         type="text"
